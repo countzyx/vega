4e333366c27efc7c1dd2fe28ba364f652e027439
Pedro R. Jiménez <countzyx@gmail.com>
2017-08-17 13:21:40 -0400

    Removed Custom Model State Error
    It didn't make sense since this is an Angular app and not an open API; there's no need to provide developer-friendly error messages.

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -21,12 +21,6 @@ namespace vega11.Controllers {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
             
-            var model = await context.Models.FindAsync(vehicleResource.ModelId);
-            if (model == null) {
-                ModelState.AddModelError("ModelId", "Invalid ModelId.");
-                return BadRequest(ModelState);
-            }
-
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
             vehicle.LastUpdate = DateTime.Now;
             context.Vehicles.Add(vehicle);
