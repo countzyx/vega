489becc34c759cb2bb7f68f32cbd9114decdafce
Pedro R. Jiménez <countzyx@gmail.com>
2017-09-10 22:42:30 -0400

    Converted Photo Constraints from File Constants to App Settings
    Created a PhotoSettings entity to hold the photo settings from the app settings file. Added a function in PhotoSettings to check that the photo file extension is supported.

--- Core/Models/PhotoSettings.cs
+++ Core/Models/PhotoSettings.cs
@@ -7,8 +7,8 @@ namespace vega11.Core.Models
         public int MaxBytes { get; set; }
         public string[] AcceptedFileTypes { get; set; }
 
-        public bool IsSupported(string fileName) {
-            return AcceptedFileTypes.Any(s => s == fileName);
+        public bool IsSupportedFileExtension(string fileExtension) {
+            return AcceptedFileTypes.Any(s => s == fileExtension);
         }
     }
 }
